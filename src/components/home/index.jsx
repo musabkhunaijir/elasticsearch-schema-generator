ff3d9a2ae4cf8dffbfd17542b194f3a4429a91da
Musab Khunaijir <musabkhunaijir@gmail.com>
2022-08-24 12:47:13 +0300

    feat: add convert button

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,13 +2,26 @@ import { SourceCode, GeneratedSchema } from "../index";
 
 export const Container = () => {
   return (
-    <div className="flex flex-row">
-      <div className="basis-1/2">
-        <SourceCode />
+    <div>
+      {/* text */}
+      <div className="flex flex-row">
+        <div className="basis-1/2">
+          <SourceCode />
+        </div>
+
+        <div className="basis-1/2">
+          <GeneratedSchema />
+        </div>
       </div>
 
-      <div className="basis-1/2">
-        <GeneratedSchema />
+      {/* button */}
+      <div className="flex justify-center">
+        <button
+          className="bg-blue-400 rounded-md h-17 w-20 text-center"
+          onClick={() => alert("clicked!")}
+        >
+          convert
+        </button>
       </div>
     </div>
   );
